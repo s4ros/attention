303277ec222c528ac4abf63ad7c4ebd4827e643f
s4ros <s4ros@infinigy.pl>
2020-03-31 22:15:35 +0200

    New version. Play mp3 generated by Google text-to-speech

--- attention.sh
+++ attention.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-# set -x
+#set -x
 
 # filename of the notification sound
 NOTIFICATION=3.mp3
-# location of the log file
-LOG=http://s4ros.it/kawusia/log.txt
 # how many times repeat the message
 LOOP=3
 # base location for config
 BASE=$(dirname $0)
 
-# get the log file and set vars
-DATA=$(curl -s $LOG)
-URL=$(echo $DATA | cut -d ' ' -f 1)
-MD5=$(echo $DATA | cut -d ' ' -f 3)
-
-# echo $URL
-# echo $MD5
+# checksum of last request.json
+MD5=$(md5sum ${BASE}/request.json | cut -d ' ' -f 1)
 
 # if this is not the first run
 if [[ -f ${BASE}/last.md5 ]]; then
-    PREVIOUS_MD5=$(cat ${BASE}/last.md5 | cut -d ' ' -f 1)
+    PREVIOUS_MD5=$(cat ${BASE}/last.md5)
 fi
 
 # if the latest md5 checksum is the same as the current
@@ -30,14 +23,12 @@ if [ ! -z ${PREVIOUS_MD5} ]; then
     test "${MD5}" == "${PREVIOUS_MD5}" && exit 0
 fi
 
-# download the file and play the attention
-curl -s $URL -o ${BASE}/attention.ogg && DOWNLOADED=1
-
 if [ ! -z ${DOWNLOADED} ]; then
     for i in $(seq 1 ${LOOP}); do
-        timeout 10 mplayer ${BASE}/sounds/${NOTIFICATION} 
-        timeout 40 ${BASE}/attention.ogg
-        sleep 3
+        #(timeout 10 mplayer -ao alsa ${BASE}/sounds/${NOTIFICATION})
+        ${BASE}/play-notification ${NOTIFICATION}
+        #("timeout 40 mplayer -ao alsa ${BASE}/attention.mp3")
+        ${BASE}/play-message
     done
 fi
 
