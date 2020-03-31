7e6d6b6af84af49ca4844b24ee7bf068f5a06746
s4ros <s4ros@infinigy.pl>
2020-03-31 23:22:42 +0200

    receiver.sh

--- receiver.sh
+++ receiver.sh
@@ -8,5 +8,5 @@ MESSAGE=${BASE}/message
 while [ 1 ]; do
     nc -l 0.0.0.0 8666 > ${MESSAGE}
     TEXT=$(cat ${MESSAGE})
-    cat ${BASE}/request-template.json | sed "s#PLACE_HERE#${TEXT}#" > ${BASE}/request.json
+    cat ${BASE}/request-template.json | sed "s#PLACE_HERE#${TEXT}#" | tee ${BASE}/request.json
 done
